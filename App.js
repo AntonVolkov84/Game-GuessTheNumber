cf04f82d0d47a0528bc3214f7ae11d6f1d8bf3f0
Anton Volkov <antvolkov84@gmail.com>
2024-09-23 20:57:54 +0300

    animated and add first list

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React, { useEffect, useState } from "react";
 import { StyleSheet, View, Alert, Text } from "react-native";
 import GuessInput from "./components/GuessInput";
 import Fireworks from "./components/Fireworks";
+import StartLevel from "./components/StartLevel";
 
 export default function App() {
   const [score, setScore] = useState(0);
+  const [start, setStart] = useState(true);
   const [level, setLevel] = useState(1);
   const pointForNextlevel = [210, 220, 230];
   const [hintCount, setHintCount] = useState(2);
   const gameDividers = [2, 5, 10, 3, 9, 4, 6, 7];
-  const [relevel, setRelevel] = useState(true);
+  const [relevel, setRelevel] = useState(false);
   const ind = (level + 2) % 3;
 
   useEffect(() => {
@@ -25,24 +27,32 @@ export default function App() {
   };
 
   return (
-    <View style={styles.container}>
-      {relevel ? (
-        <Fireworks level={level} gameDeviders={gameDividers} setRelevel={setRelevel} />
+    <>
+      {start ? (
+        <View style={{ paddingTop: 50, paddingHorizontal: 20 }}>
+          <StartLevel setStart={setStart} level={level} gameDeviders={gameDividers} />
+        </View>
       ) : (
-        <>
-          <GuessInput
-            setHintCount={setHintCount}
-            hintCount={hintCount}
-            onScoreUpdate={handleScoreUpdate}
-            level={level}
-            gameDeviders={gameDividers}
-          />
-          <Text style={styles.scoreText}>
-            Очки: {score} / {pointForNextlevel[ind]}
-          </Text>
-        </>
+        <View style={styles.container}>
+          {relevel ? (
+            <Fireworks level={level} gameDeviders={gameDividers} setRelevel={setRelevel} />
+          ) : (
+            <>
+              <GuessInput
+                setHintCount={setHintCount}
+                hintCount={hintCount}
+                onScoreUpdate={handleScoreUpdate}
+                level={level}
+                gameDeviders={gameDividers}
+              />
+              <Text style={styles.scoreText}>
+                Очки: {score} / {pointForNextlevel[ind]}
+              </Text>
+            </>
+          )}
+        </View>
       )}
-    </View>
+    </>
   );
 }
 
