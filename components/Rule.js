6d91e868794079d050390459205062978bf57734
Anton Volkov <antvolkov84@gmail.com>
2024-09-28 12:39:21 +0300

    Rule

--- components/Rule.js
+++ components/Rule.js
@@ -1,10 +1,83 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
+import { useTranslation } from "react-i18next";
+import styled from "styled-components";
+import { LinearGradient } from "expo-linear-gradient";
+
+const ButtonAgryLanguage = styled.TouchableOpacity`
+  width: 150px;
+  height: 60px;
+  margin: 0 auto;
+  border-radius: 28px;
+  margin-top: 50px;
+`;
+const ButtonText = styled.Text`
+  color: whitesmoke;
+  font-size: 15px;
+  text-align: center;
+`;
+const RuleBlock = styled.View`
+  flex-direction: row;
+  gap: 10px;
+  margin-top: 5%;
+  justify-content: center;
+  align-items: center;
+`;
+const RuleBlockImage = styled.Image`
+  width: 50%;
+  height: 200px;
+  border-radius: 18px;
+`;
+const RuleBlockText = styled.Text`
+  display: block;
+  width: 47%;
+  height: content;
+  border-radius: 18px;
+  color: whitesmoke;
+  padding: 10px;
+  font-size: 22px;
+  text-align: center;
+`;
 
 export default function Rule() {
+  const { t } = useTranslation();
   return (
-    <View>
-      <Text>Rule</Text>
-    </View>
+    <LinearGradient
+      colors={["#1E2322", "#1F433A", "#1E2322", "#1F433A"]}
+      start={{ x: 0.0, y: 0.0 }}
+      end={{ x: 1.0, y: 1.0 }}
+      style={{ height: "100%", width: "100%", padding: 10, marginTop: 40 }}
+    >
+      <RuleBlock>
+        <RuleBlockImage source={require("../assets/Component 1.png")}></RuleBlockImage>
+        <RuleBlockText>{t("Rule Component1")}</RuleBlockText>
+      </RuleBlock>
+      <RuleBlock>
+        <RuleBlockText>{t("Rule Component2")}</RuleBlockText>
+        <RuleBlockImage source={require("../assets/Component 2.png")}></RuleBlockImage>
+      </RuleBlock>
+      <RuleBlock>
+        <RuleBlockImage source={require("../assets/Component 3.png")}></RuleBlockImage>
+        <RuleBlockText>{t("Rule Component3")}</RuleBlockText>
+      </RuleBlock>
+      <ButtonAgryLanguage>
+        <LinearGradient
+          colors={["#849ae9", "#6ea0eb", "#2db3f1", "#2ab4f1"]}
+          start={{ x: 0.0, y: 0.0 }}
+          end={{ x: 1.0, y: 1.0 }}
+          style={{
+            height: "100%",
+            width: "100%",
+            padding: 10,
+            overflow: "hidden",
+            borderRadius: 30,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <ButtonText>{t("Rule button")}</ButtonText>
+        </LinearGradient>
+      </ButtonAgryLanguage>
+    </LinearGradient>
   );
 }
